9074533f4dc0a167b030496e259bf5fbb8fb158e
raheelhparekh <rhparekh2003@gmail.com>
2025-02-06 20:22:28 +0530

    updated date currency data automatically generated

--- 05_currencyConverter/src/hooks/useCurrencyInfo.js
+++ 05_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -4,15 +4,16 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   const [date, setDate] = useState('');
+  const today = new Date().toISOString().split('T')[0]; // get today's date
 
   useEffect(() => {
-    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-01-31/v1/currencies/${currency}.json`)
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${today}/v1/currencies/${currency}.json`)
       .then((res) => res.json()) // converts string to json
       .then((res) => {
         setData(res[currency]);
         setDate(res.date);
       });
-  }, [currency]);
+  }, [currency, today]);
   console.log(data);
 
   return { data, date };
